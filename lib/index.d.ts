74e3375827c17cd9a1fcef91bf42742f7568bd98
zoolsher <zoolsher@gmail.com>
2018-09-20 17:26:24 +0800

    feat: esm

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import genetic, { Genetic } from './genetic';
-import MaxRectBinPack, { FindPosition } from './max-rect-bin-pack';
-import Rect from './rect';
-import { Search } from './search';
-export { FindPosition, Genetic, // 遗传算法类
-MaxRectBinPack, Rect, genetic, // 包装方法
-Search, };
